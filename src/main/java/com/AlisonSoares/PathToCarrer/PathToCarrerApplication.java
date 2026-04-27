537c816c955161bd9efc606e7a71f862e511cf1d
Alison Martins <alisonmarteso@gmail.com>
2026-04-27 20:58:59 -0300

    Dependencias iniciais !

---
+++ src/main/java/com/AlisonSoares/PathToCarrer/PathToCarrerApplication.java
@@ -0,0 +1,13 @@
+package com.AlisonSoares.PathToCarrer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathToCarrerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathToCarrerApplication.class, args);
+	}
+
+}
