e5811b34bf3b9df46029f1cc928ac933a3728bd8
Alison Martins <alisonmarteso@gmail.com>
2026-05-10 23:44:26 -0300

    feat: Dominio usuario atualizado

---
+++ src/main/java/com/AlisonSoares/PathToCarrer/model/usuariodDomain/ConteudoConsumido.java
@@ -0,0 +1,6 @@
+package com.AlisonSoares.PathToCarrer.model.usuariodDomain;
+
+public class ConteudoConsumido {
+    private Long usuarioID;
+    private Long conteudoID;
+}
---
+++ src/main/java/com/AlisonSoares/PathToCarrer/model/usuariodDomain/ExerciciosRealizados.java
@@ -0,0 +1,12 @@
+package com.AlisonSoares.PathToCarrer.model.usuariodDomain;
+
+import java.time.LocalDate;
+
+public class ExerciciosRealizados {
+    private Long usuarioID;
+    private Long exercicioID;
+    private Long conteudoID;
+    private String opcaoMarcada;
+    private boolean acertou;
+    private  LocalDate ultimoContato;
+}
